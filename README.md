68a41d0aa76800a6f97cab8e41c2a332b8ea869a
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-24 09:54:37 -0400

    adds challenges

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Fundamental Functions
+
+### A Function Fundamentals Challenge
+
+All directions are in comment form in main.js!
+
+* console.log
+* function that calls another function
+* 
---
+++ main.js
@@ -0,0 +1,45 @@
+const seanBeanMovies = [
+  'The Martian',
+  'Percy Jackson & the Olympians: The Lightning Thief',
+  'Far North',
+  'The Island',
+  'National Treasure',
+  'Troy',
+  'The Lord of The Rings: The Return of the King',
+  'Equilibrium',
+  'The Lord of the Rings: The Two Towers',
+  'The Lord of The Rings: The Fellowship of the Ring',
+  'Anna Karenina',
+  'Goldeneye'
+];
+
+const seanBeanShows = [
+  'Curfew',
+  'Medici',
+  'Wasted',
+  'Missing',
+  'Legends',
+  'Game of Thrones',
+  'Crusoe',
+  'Clarissa',
+  'The Practice',
+]
+
+console.log('# Challenge #1');
+// Create a function that console.logs every item in the movie list, then call that function.
+
+
+console.log('# Challenge #1');
+// Create a function that console.logs every item in the TV list, then call that function.
+
+
+console.log('# Challenge #1');
+// There's an order to the array items, but only if you're a major tv or movie buff--or, like this project's author, a minor Sean Bean buff--would you notice; they're listed chronologically, newest first. Now write a function that consoles out the movies in oldest-first order.
+
+
+console.log('# Challenge #1');
+// Now do the same as above, but for TV shows.
+
+
+console.log('# Challenge #1');
+// Now that we know we've got 
